a9e918688b60038f531ea08956ae342d13fb2e2e
William Queiroz <wnqueiroz@outlook.com>
2022-05-22 20:29:01 -0300

    feat: add boilerplate

---
+++ README.md
@@ -0,0 +1,7 @@
+# udemy-react-avancado
+
+## boilerplate
+
+```sh
+yarn create next-app -e https://github.com/wnqueiroz/udemy-react-avancado/tree/main/boilerplate
+```
---
+++ boilerplate/next.config.js
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withPWA = require('next-pwa')
+const isProd = process.env.NODE_ENV === 'production'
+
+module.exports = withPWA({
+  reactStrictMode: true,
+  pwa: {
+    dest: 'public',
+    disable: !isProd
+  }
+})
