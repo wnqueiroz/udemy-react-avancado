28ebc759e796cfb7c8c4dc75617dd6e666bb3e76
William Queiroz <wnqueiroz@outlook.com>
2022-09-06 22:26:15 -0300

    feat(landing-page-ui): create the first GraphQL query

---
+++ landing-page-ui/src/graphql/queries/get-landing-page.ts
@@ -0,0 +1,46 @@
+import { gql } from 'graphql-request'
+
+export const GET_LANDING_PAGE = gql`
+  fragment imageData on UploadFileEntityResponse {
+    data {
+      attributes {
+        url
+        alternativeText
+      }
+    }
+  }
+
+  fragment logo on LandingPage {
+    logo {
+      ...imageData
+    }
+  }
+
+  fragment header on LandingPage {
+    header {
+      title
+      description
+      button {
+        label
+        url
+      }
+      image {
+        ...imageData
+      }
+    }
+  }
+
+  query GET_LANDING_PAGE {
+    landingPage {
+      data {
+        attributes {
+          # alias
+          created_at: createdAt
+          ...logo
+
+          ...header
+        }
+      }
+    }
+  }
+`
