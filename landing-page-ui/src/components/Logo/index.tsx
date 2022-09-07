2fed737754164e7651f3c712d13e0ff12b688a53
William Queiroz <wnqueiroz@outlook.com>
2022-09-07 16:06:29 -0300

    feat(landing-page-ui): use environment variables

--- landing-page-ui/src/components/Logo/index.tsx
+++ landing-page-ui/src/components/Logo/index.tsx
@@ -1,13 +1,12 @@
 import React from 'react'
 import { LogoProps } from '../../types/api'
+import { getImageUrl } from '../../utils/get-image-url'
 import * as S from './styles'
 
 const Logo = ({
   data: {
     attributes: { alternativeText, url }
   }
-}: LogoProps) => (
-  <S.LogoWrapper src={`http://localhost:1337${url}`} alt={alternativeText} />
-)
+}: LogoProps) => <S.LogoWrapper src={getImageUrl(url)} alt={alternativeText} />
 
 export default Logo
