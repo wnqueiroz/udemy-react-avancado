0767e780ffc928a18b2cd4f6f412df3b31aeb5d1
William Queiroz <wnqueiroz@outlook.com>
2022-09-12 19:28:25 -0300

    feat(won-games-api): get game info using JSDOM to scrape page data

--- won-games-api/src/api/game/services/populate.ts
+++ won-games-api/src/api/game/services/populate.ts
@@ -3,6 +3,21 @@
  */
 
 import axios from "axios";
+import { JSDOM } from "jsdom";
+
+async function getGameInfo(slug: string) {
+  const body = await axios.get(`https://www.gog.com/game/${slug}`);
+
+  const dom = new JSDOM(body.data);
+
+  const description = dom.window.document.querySelector(".description");
+
+  return {
+    rating: "BR0",
+    short_description: description.textContent.slice(0, 160),
+    description: description.innerHTML,
+  };
+}
 
 export default async (ctx) => {
   const gogApiUrl = `https://www.gog.com/games/ajax/filtered?mediaType=game&page=1&sort=popularity`;
@@ -11,7 +26,9 @@ export default async (ctx) => {
     data: { products },
   } = await axios.get(gogApiUrl);
 
-  console.log(products);
+  const gameInfo = await getGameInfo(products[0].slug);
+
+  console.log(gameInfo);
 
   return {
     john: "doe",
