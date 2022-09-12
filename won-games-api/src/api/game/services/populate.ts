f2d8c71a4ee826d36eeaeb919f1595c62a1452b5
William Queiroz <wnqueiroz@outlook.com>
2022-09-12 19:14:31 -0300

    feat(won-games-api): call GoG API to get game data

--- won-games-api/src/api/game/services/populate.ts
+++ won-games-api/src/api/game/services/populate.ts
@@ -2,8 +2,16 @@
  * populate service
  */
 
+import axios from "axios";
+
 export default async (ctx) => {
-  console.log(ctx);
+  const gogApiUrl = `https://www.gog.com/games/ajax/filtered?mediaType=game&page=1&sort=popularity`;
+
+  const {
+    data: { products },
+  } = await axios.get(gogApiUrl);
+
+  console.log(products);
 
   return {
     john: "doe",
